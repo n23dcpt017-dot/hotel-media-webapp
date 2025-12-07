29463a89c2b1d2995304a50d70e880b5e6a1fd0c
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-08 00:04:47 +0700

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,7 +8,8 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/hotel_media'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
