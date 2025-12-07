25d0a9b4cc83e536ba84af8b312ef7498c360125
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-07 23:16:32 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,16 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app():
+def create_app(config_name='default'):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+
+    if config_name == 'testing':
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
