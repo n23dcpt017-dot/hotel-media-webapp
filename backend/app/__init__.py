1fe88e04bc22cd5b527767d5cc0b15ffc27a0e20
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-08 00:11:21 +0700

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
-'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
