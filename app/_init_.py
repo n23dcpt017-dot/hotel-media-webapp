1150d421701ade5284c20815137e6da5238ee4b9
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-07 22:44:53 +0700

    Update _init_.py

--- app/_init_.py
+++ app/_init_.py
@@ -1,28 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from app import create_app, db
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+app = create_app()
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-    
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    
-    # Đăng ký blueprints
-    from app.routes import auth, dashboard, baiviet, binhluan, chienich, nguoidung, quanly, media
-    
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(dashboard.bp)
-    app.register_blueprint(baiviet.bp)
-    app.register_blueprint(binhluan.bp)
-    app.register_blueprint(chienich.bp)
-    app.register_blueprint(nguoidung.bp)
-    app.register_blueprint(quanly.bp)
-    app.register_blueprint(media.bp)
-    
-    return app
+with app.app_context():
+    db.create_all()
