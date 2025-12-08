abb093d19b9c5c57f6ea639e02a0ca34d0c0b554
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-08 21:17:53 +0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
 from app.models.user import User
 
-auth = Blueprint('auth', _name_)
+auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
