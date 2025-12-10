c09eaff01d764321af8afe3a92bc28451f931c08
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-11 01:46:17 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-
 # ========================
 # Seed user for Selenium
 # ========================
@@ -44,25 +43,16 @@ def create_app(config_name=None):
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
 
-
     # Register blueprints
     from app.routes import auth
     app.register_blueprint(auth, url_prefix='/auth')
 
-    # Setup DB + optional seed
+    # Setup DB + seed
     with app.app_context():
         db.create_all()
+        seed_selenium_user()
 
-        
-       # Setup DB + optional seed
-with app.app_context():
-    db.create_all()
-
-    # ✅ LUÔN seed kể cả khi test
-    seed_selenium_user()
-
-
-    
+    # Flask-Login user loader
     from app.models.user import User
 
     @login_manager.user_loader
