84363a3801987ba92a79fc8406132d7fbf131d61
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-17 00:50:07 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from datetime import datetime
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-# ========================
-# Seed user
-# ========================
+
 def seed_selenium_user():
     from app.models.user import User
-
     if User.query.first():
         return
-
+    
     admin = User(
         username="admin@hotel.com",
         email="admin@hotel.com",
@@ -25,48 +23,10 @@ def seed_selenium_user():
     db.session.commit()
 
 
-
-
-# ========================
-# Application Factory
-# ========================
-def create_app(config_name=None):
-    app = Flask(__name__, template_folder='../templates', static_folder='../static')
-
-    app.config['SECRET_KEY'] = 'dev'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    if config_name == 'testing':
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
-
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = "auth.login"
-
-    from app.routes import auth
-    app.register_blueprint(auth, url_prefix='/auth')
-
-    with app.app_context():
-        
-        from app.models.user import User
-        from app.models.post import Post
-        from app.models.comment import Comment
-        from app.models.campaign import Campaign
-        from app.models.media import Media
-
-        db.create_all()
-        seed_sample_posts()
-        seed_selenium_user()
-        
-from datetime import datetime
-
 def seed_sample_posts():
     from app.models.post import Post
-    from app import db
-
+    
+    
     if Post.query.first():
         return
 
@@ -116,9 +76,41 @@ def seed_sample_posts():
     db.session.add_all(samples)
     db.session.commit()
 
+def create_app(config_name=None):
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
+    app.config['SECRET_KEY'] = 'dev'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if config_name == 'testing':
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
+    
     @login_manager.user_loader
     def load_user(user_id):
+        from app.models.user import User
         return User.query.get(int(user_id))
 
-    return app
+    
+    from app.routes import auth
+    app.register_blueprint(auth, url_prefix='/auth')
+
+    
+    with app.app_context():
+        
+        from app.models.user import User
+        from app.models.post import Post
+        
+        
+        db.create_all()         
+        seed_sample_posts()     
+        seed_selenium_user()    
+        
+    return app  
