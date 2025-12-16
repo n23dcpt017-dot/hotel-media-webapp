c4ef3425a9d27f10522909feb51b3ad70df975c5
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-17 00:39:32 +0700

    Update post.py

--- app/models/post.py
+++ app/models/post.py
@@ -7,6 +7,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text)
+    author = db.Column(db.String(100))
     status = db.Column(
         db.String(20),
         default="draft"  # draft | published | scheduled
