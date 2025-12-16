4249db53d80855053d4654c5ee9a838662a1cd11
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-16 20:36:28 +0700

    Create post.py

---
+++ app/models/post.py
@@ -0,0 +1,16 @@
+from app import db
+from datetime import datetime
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    is_hidden = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
