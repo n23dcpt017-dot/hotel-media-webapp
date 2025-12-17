1057fe6b54162fdefcea92cf6b6de5ba6b404ecf
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-17 20:36:19 +0700

    Update media.py

--- app/models/media.py
+++ app/models/media.py
@@ -2,9 +2,11 @@ from app import db
 from datetime import datetime
 
 class Media(db.Model):
+    __tablename__ = "media"
+    
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255))
-    url = db.Column(db.String(255))
-    type = db.Column(db.String(20))   # image | video
+    filename = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(20))  # image | video
     created_at = db.Column(db.DateTime, default=db.func.now())
+
 
