0c1c6b4b6f7528f67674187bc6ecc4842b8ff704
Chống Đặng Chí Thành <n23dcpt047@student.ptithcm.edu.vn>
2025-12-17 19:46:29 +0700

    Update media.py

--- app/models/media.py
+++ app/models/media.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255), nullable=False)
+    filename = db.Column(db.String(255))
+    url = db.Column(db.String(255))
+    type = db.Column(db.String(20))   # image | video
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
-    is_hidden = db.Column(db.Boolean, default=False)
-
-    uploaded_at = db.Column(db.DateTime, default=datetime.utcnow)
